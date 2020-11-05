a0b837dc67451d74aeb2239e91f62da742ef4524
ShigemichiMatsuzaki <matsuzaki@aisl.cs.tut.ac.jp>
2020-11-05 22:05:18 +0900

    [modify] Remove unnecessary lines

--- model/segmentation/espdnet_mult.py
+++ model/segmentation/espdnet_mult.py
@@ -14,41 +14,6 @@ from model.classification.espnetv2 import EESPNet
 from utilities.print_utils import *
 from torch.nn import functional as F
 
-#class FusionGate(nn.Module):
-#    '''
-#    Gate to fuse RGB and depth features
-#    '''
-#    def __init__(self, nchannel):
-#        super().__init__()
-#
-#        # Channel size of input features (must be the same)
-#        self.nchannel = nchannel
-#
-#        self.conv_1x1 = C(nIn=2 * self.nchannel, nOut=self.nchannel, kSize=1)
-#        self.sigmoid  = torch.nn.Sigmoid()
-#        
-#    def forward(self, rgb, depth, naive_fuse=False):
-#        if naive_fuse:
-#            output = rgb + depth
-#        else:
-#            # 1. Concat RGB and depth features
-#            output = torch.cat((rgb, depth), 1)
-#    
-#            # 2. 1x1 convolution
-#            output = self.conv_1x1(output)
-#    
-#            # 3. Sigmoid
-#            weight = self.sigmoid(output)
-#    
-#            # 4. Multiply each features by the yielded weights
-#            size = weight.size()
-#            w_rgb = rgb * weight
-#            w_depth = depth * (torch.ones(size).to('cuda') - weight)
-#    
-#            output = w_rgb + w_depth
-#
-#        return output
-
 class ESPDNetSegCls(nn.Module):
     '''
     This class defines the ESPDNet architecture for the multi task training of Semantic Segmenation and classification
--- model/segmentation/espdnet_ue.py
+++ model/segmentation/espdnet_ue.py
@@ -15,46 +15,6 @@ from utilities.print_utils import *
 from torch.nn import functional as F
 import copy
 
-#class FusionGate(nn.Module):
-#    '''
-#    Gate to fuse RGB and depth features
-#    '''
-#    def __init__(self, nchannel, is_trainable=True):
-#        super().__init__()
-#
-#        # Channel size of input features (must be the same)
-#        self.nchannel = nchannel
-#
-#        self.conv_1x1 = C(nIn=2 * self.nchannel, nOut=self.nchannel, kSize=1)
-#        self.sigmoid  = torch.nn.Sigmoid()
-#
-#        self.is_trainable = is_trainable
-#        
-#    def forward(self, rgb, depth):
-#        if self.is_trainable:
-#    #      print(rgb.size())
-#    #      print(depth.size())
-#          # 1. Concat RGB and depth features
-#          output = torch.cat((rgb, depth), 1)
-#  
-#          # 2. 1x1 convolution
-#          output = self.conv_1x1(output)
-#  
-#          # 3. Sigmoid
-#          weight = self.sigmoid(output)
-#  
-#          # 4. Multiply each features by the yielded weights
-#          size = weight.size()
-#          w_rgb = rgb * weight
-#          w_depth = depth * (torch.ones(size).to('cuda') - weight)
-#  
-#          output = w_rgb + w_depth
-#
-#        else:
-#          output = rgb + depth
-#
-#        return output
-
 class ESPDNetwithUncertaintyEstimation(nn.Module):
     '''
     This class defines the ESPDNet architecture for the Semantic Segmenation with uncertainty estimation
